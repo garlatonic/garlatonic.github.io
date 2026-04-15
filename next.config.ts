6d0e7a1784e26a7ce8fbc331861bdf6784425471
ivoryShield <sangapark34@naver.com>
2026-04-15 16:00:39 +0900

    feat: webpack 설정에 SVG 로더 추가

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,13 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   output: "export",
   reactStrictMode: true,
   images: {
