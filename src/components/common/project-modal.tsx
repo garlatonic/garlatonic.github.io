fd4781d52e00f84fab8bee381bb903bc94a9349a
ivoryShield <sangapark34@naver.com>
2026-04-23 00:05:22 +0900

    fix: 불필요한 import 문 제거 및 코드 정리

--- src/components/common/project-modal.tsx
+++ src/components/common/project-modal.tsx
@@ -1,21 +1,7 @@
-"use client";
-
+"use client";;
 import { useEffect } from "react";
 import { projects } from "@/data/projects";
-import { ExternalLinkIcon } from "lucide-react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import Video from "@/components/ui/video";
-import GithubIcon from "@/components/common/github-icon";
-import SectionTitle from "@/components/common/section-title";
-import SectionWrapper from "@/components/common/section-wrapper";
-import ImageGrid from "@/app/projects/[slug]/_components/image-grid";
-import Description from "@/app/projects/[slug]/_components/description";
-import TroubleShooting from "@/components/project-detail/trouble-shooting";
-import { Fragment } from "react/jsx-runtime";
 import Modal from "./modal";
-import Heading from "../project-detail/heading";
 import ProjectDetail from "../project-detail";
 
 type ProjectModalProps = {
