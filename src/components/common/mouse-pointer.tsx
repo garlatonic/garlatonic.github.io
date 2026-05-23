9cb99996aeafb905681c5e62f2b679a08b488f9b
ivoryShield <sangapark34@naver.com>
2026-05-23 23:57:38 +0900

    fix: 마우스 포인터 z-index 값 변경

--- src/components/common/mouse-pointer.tsx
+++ src/components/common/mouse-pointer.tsx
@@ -58,7 +58,7 @@ export default function MousePointer() {
     <div
       style={{ left: x, top: y }}
       className={twMerge(
-        "pointer-events-none fixed z-50 size-6 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-white mix-blend-difference transition-[width,height] duration-150 ease-out",
+        "pointer-events-none fixed z-100 size-6 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-white mix-blend-difference transition-[width,height] duration-150 ease-out",
         cursor === "pointer" && "size-10",
       )}
     >
